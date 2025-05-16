9c138a44e960c772062e014ed4b36780131ae397
Jesse <jesse.caddell@wsu.edu>
2025-05-15 20:52:59 -0700

    Initial commit

---
+++ recipe-gallery/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RecipeGallery from './components/RecipeGallery';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <h1>Recipe Gallery</h1>
+        <RecipeGallery />
+      </div>
+  );
+}
+
+export default App;
+
